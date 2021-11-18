7c32f470bd70e704fa865962b73e7eada9c4ec51
Vinicius Pani <viniciuspani@hotmail.com>
2021-11-18 00:23:29 -0300

    Tela login com teste

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,13 @@
 let userSistema = document.getElementById(inputLogin);
 let userPassword = document.getElementById(inputPassword);
 
-var user = vinicius;
+var user = "vinicius";
 var senha = 123;
 
 function VerificaUsuario(user, senha) {
     if (user === userSistema && senha === userPassword) {
-        alert("Usuario logado com sucesso!")
+        alert("usuario logado!");
+        windows.location.replace = ("//assets/sistemaOficios.html");
     } else {
         alert("usuario incorreto!");
     }
