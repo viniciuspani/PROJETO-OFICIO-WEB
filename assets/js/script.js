a6781ded4c4e1cc61d2e5041ad7ac8be916d08be
Vinicius Pani <viniciuspani@hotmail.com>
2021-11-22 16:58:34 -0300

    Atualizacao script

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,13 @@
-let userSistema = document.getElementById(inputLogin);
-let userPassword = document.getElementById(inputPassword);
+var userSistema = document.getElementById(inputLogin);
+var userPassword = document.getElementById(inputPassword);
 
 var user = "vinicius";
 var senha = 123;
 
 function VerificaUsuario(user, senha) {
-    if (user === userSistema && senha === userPassword) {
+    if (user == userSistema && senha == userPassword) {
         alert("usuario logado!");
-        window.location.href="https://sistemaOficios.html";
+        window.location.href="https://sistemaOficios.html:5500";
         
     } else {
         alert("usuario incorreto!");
