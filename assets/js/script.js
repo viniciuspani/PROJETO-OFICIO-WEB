f74b1c42867c6a349245a7b9fcc16a86901f9537
Vinicius Pani <viniciuspani@hotmail.com>
2022-02-02 13:12:21 -0300

    Atualizacao login
    Mudança na funcao de login

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,22 @@
-var userSistema = document.getElementById(inputLogin);
-var userPassword = document.getElementById(inputPassword);
+const LOGIN_USUARIO = "usuario";
+const SENHA_USUARIO = "123logar";
 
-var user = "vinicius";
-var senha = 123;
+function loginPagina(){
+    debugger
 
-function VerificaUsuario(user, senha) {
-    if (user == userSistema && senha == userPassword) {
-        alert("usuario logado!");
-        window.location.href="https://sistemaOficios.html:5500";
-        
-    } else {
-        alert("usuario incorreto!");
-    }
+    var email = $("#loginUsuario").val();
+    var passoword = $("#passwordUsuario").val();
+
+    if ((email === LOGIN_USUARIO) && (passoword === SENHA_USUARIO))  {        
+        //toastr.success('Usuario encontrado');
+        //setTimeout(function(){location.href="sistemaOficios.html"} , 5000);   
+        $(location).attr('href', 'sistemaOficios.html');
+    }else if (email != LOGIN_USUARIO){
+        //toastr.error('Email inválido!');
+        console.log('Usuario incorreto')
+    } else if(passoword != SENHA_USUARIO){
+        //toastr.error('Senha inválida!');
+        console.log('Senha incorreto')
+    }        
+
 }
